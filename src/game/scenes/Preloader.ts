dbb566f1b7aa50abe44ebca8b3374b4fb4632c39
Miles Edwards <miles.mfe@gmail.com>
2025-09-24 18:15:05 +0100

    Concat chart images into one

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,25 +1,20 @@
 import { Scene } from 'phaser';
 import { TrackData, TrackSpaceType } from '../TrackData';
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
+export class Preloader extends Scene {
+    constructor() {
         super('Preloader');
     }
 
-    init ()
-    {
+    init() {
         // This method is called first. You can use it to prepare data.
     }
 
-    preload ()
-    {
+    preload() {
         // Load Assets
         this.load.json('track-data', 'assets/track-data.json');
         this.load.image('track', 'assets/track.png');
-        this.load.image('penalty-chart', 'assets/penalty-chart.png');
-        this.load.image('speed-reduction-chart', 'assets/speed-reduction-chart.png');
+        this.load.image('charts', 'assets/charts.png');
         this.load.image('red-car', 'assets/car-red.png');
         this.load.image('purple-car', 'assets/car-purple.png');
         this.load.image('green-car', 'assets/car-green.png');
@@ -34,7 +29,7 @@ export class Preloader extends Scene
         const progressBox = this.add.graphics();
         progressBox.fillStyle(0x222222, 0.8);
         progressBox.fillRect(width / 4, height / 2 - 30, width / 2, 50);
-        
+
         const loadingText = this.make.text({
             x: width / 2,
             y: height / 2 - 50,
@@ -55,14 +50,14 @@ export class Preloader extends Scene
             }
         });
         percentText.setOrigin(0.5, 0.5);
-        
+
         this.load.on('progress', (value: number) => {
             percentText.setText(Math.round(value * 100) + '%');
             progressBar.clear();
             progressBar.fillStyle(0xffffff, 1);
             progressBar.fillRect(width / 4 + 10, height / 2 - 20, (width / 2 - 20) * value, 30);
         });
-        
+
         this.load.on('complete', () => {
             progressBar.destroy();
             progressBox.destroy();
@@ -71,8 +66,7 @@ export class Preloader extends Scene
         });
     }
 
-    create ()
-    {
+    create() {
         const trackData: TrackData = this.cache.json.get('track-data');
         const topography = trackData.topography;
 
