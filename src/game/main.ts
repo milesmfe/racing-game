4a7ab3c9e64cf08d2c872f5b1add9abf8459f51a
Miles Edwards <miles.mfe@gmail.com>
2025-10-07 15:36:22 +0100

    Fix scaling and resolution bug

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,18 +1,25 @@
 import { Boot } from './scenes/Boot';
-import { AUTO, Game } from 'phaser';
+import { AUTO, Game, Scale } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 import { GameLobby } from './scenes/GameLobby';
 import { GameScene } from './scenes/GameScene';
 import { GameEndScene } from './scenes/GameEndScene';
 
+const GAME_WIDTH = 1920;
+const GAME_HEIGHT = 1080;
+
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
     parent: 'game-container',
     backgroundColor: '#2b6336',
     dom: {
         createContainer: true,
+    },
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH,
     },
     scene: [
         Boot,
@@ -27,13 +34,6 @@ let game: Phaser.Game | null = null;
 
 const StartGame = (parent: string) => {
     game = new Game({ ...config, parent });
-
-    // Add resize event listener
-    window.addEventListener('resize', () => {
-        if (game) {
-            game.scale.resize(window.innerWidth, window.innerHeight);
-        }
-    });
 
     return game;
 }
